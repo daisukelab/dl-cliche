34ab9923ed4923b7840513a215d8362959dbacc5
daisukelab <niz.edge@gmail.com>
2018-11-09 08:53:00 +0900

    Fix notebook detection.

--- dlcliche/notebook.py
+++ dlcliche/notebook.py
@@ -3,11 +3,15 @@ from IPython import get_ipython
 ipython = get_ipython()
 
 # Determine if this is running in Jupyter notebook or not
-running_in_notebook = ipython.has_trait('kernel')
+if ipython:
+    running_in_notebook = ipython.has_trait('kernel')
 
-if running_in_notebook:
-    ipython.magic('reload_ext autoreload')
-    ipython.magic('autoreload 2')
-    ipython.magic('matplotlib inline')
+    if running_in_notebook:
+        ipython.magic('reload_ext autoreload')
+        ipython.magic('autoreload 2')
+        ipython.magic('matplotlib inline')
+else:
+    # cannot even get ipython object...
+    running_in_notebook = False
 
 # Didn't work -> https://stackoverflow.com/questions/32906669/how-to-use-ipython-magic-within-a-script-to-auto-reload-modules
