0d9b13e4fffa02bc8455dbc733024d2afac1724c
daisukelab <niz.edge.gmail.com>
2018-12-20 13:31:18 +0900

    Update project_readme, add image utilities.

--- PROJECT_README.md
+++ PROJECT_README.md
@@ -1,49 +1,53 @@
-# project-ml
+# Project XXX
 Template for general machine learning project.
 
-## Project Setup
-
-### Description
+## About this project
 Some more project description, preferably define followings:
 
-- Goal, what should be achieved by this project.
-- Resource, what are resources expected to be available to this project.
-- Target users.
+- Objectives/Goals, what should be achieved by this project.
+- Resources, what are resources expected to be available to this project.
+- Target scope.
+- Concerns.
+- Expectations, what could this project yield or address.
+
+## Status
+- Short summary, if available.
+- Issues/Problems.
+- Remainings/Todos/Future Works.
+
+## Goal List
+- [ ] 1. XXX.
+- [ ] 2. YYY.
+- [ ] 3. ZZZ.
+
+## Plan
+- By DD-MM Do this.
+- By DD-MM Do that.
+
+## Storategy
+What are actions to achive goal by using what resources and following policies.
+
+- Initial preparations.
+- Process/Flow.
 - Metrics.
+- Criterion.
 
-### Strategy
-What are actions to achive goal by using what resources and policies.
+## Process detail.
+- Dataset policy, how to handle dataset.
+- Training policy, how to train models.
+- Parameter policy, what are parameters, and policies for them.
+- Development cycle
+    - Update parameters for this cycle under current policies.
+    - Setup data for this cycle under current parameters.
+    - Train models under current parameters and policies.
+    - Evaluate returns earned by models trained in this cycle.
+    - Update all the policies to prepare for the next cycle.
+      Or make a decision to finish development cycle.
+    - When finished, summarize all the results and evaluate system performance.
 
-### Dataset Policy
-How to handle dataset.
-
-### Training Policy
-How to train models.
-
-### Parameter Policy
-What are parameters, and policies for them.
-
-## Development Cycle Setup
-
-### Update Parameter
-Update parameters for this cycle under current policies.
-
-### Setup Data
-Setup data for this cycle under current parameters.
-
-### Train Model
-Train models under current parameters and policies.
-
-### Evaluate Return
-Evaluate returns earned by models trained in this cycle.
-
-### Update Policy
-Update all the policies to prepare for the next cycle.
-
-Or make a decision to finish development cycle.
-
-## Project Summary
-
-### Evaluate Total Performance
-Summarize all the results and evaluate system performance.
+## Summary
+- Results.
+- Takeaways.
+- Lessons.
+- Reports.
 
--- dlcliche/image.py
+++ dlcliche/image.py
@@ -13,7 +13,7 @@ def _resize_worker(dest_folder, list_of_files, shape):
         results.append((outfile, (img.shape[1], img.shape[0])))
     return results
 
-def resize_image_files(dest_folder, source_files, shape=(224, 224), num_threads=4):
+def resize_image_files(dest_folder, source_files, shape=(224, 224), num_threads=4, skip_if_any_there=False):
     """Make resized copy of listed images in parallel processes.
 
     Arguments:
@@ -21,9 +21,10 @@ def resize_image_files(dest_folder, source_files, shape=(224, 224), num_threads=
         source_files: Source image files.
         shape: (Width, Depth) shape of copies.
         num_threads: Number of parallel workers.
+        skip_if_any_there: If True, skip processing processing if any file have already been done.
 
     Returns:
-        List of image info (filename, original size) tuples.
+        List of image info (filename, original size) tuples, or None if skipped.
         ex)
         ```python
         [('tmp/8d6ed7c786dcbc93.jpg', (1024, 508)),
@@ -31,6 +32,9 @@ def resize_image_files(dest_folder, source_files, shape=(224, 224), num_threads=
          ('tmp/8d6f00feedb09efa.jpg', (1024, 683))]
         ```
     """
+    if skip_if_any_there:
+        if (Path(dest_folder)/Path(source_files[0]).name).exists():
+            return None
     with Pool(num_threads) as p:
         ns = len(source_files) // num_threads
         ensure_folder(dest_folder)
@@ -38,6 +42,25 @@ def resize_image_files(dest_folder, source_files, shape=(224, 224), num_threads=
                                                      else source_files[ns*(num_threads-1):], shape)
                                              for i in range(num_threads)])
         return flatten_list(returns)
+
+def load_rgb_image(filename):
+    """Load image file and make sure that format is RGB."""
+    img = cv2.imread(str(filename))
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    return img
+
+def render_bbox(img, bbox, class_name, color=(255, 0, 0), text_color=(255, 255, 255), thickness=1):
+    """Object Detection Helper: Render single bounding box with class name on top of image.
+    Thanks to https://github.com/facebookresearch/Detectron/blob/master/detectron/utils/vis.py
+    """
+    x_min, y_min, w, h = bbox
+    x_min, x_max, y_min, y_max = int(x_min), int(x_min + w), int(y_min), int(y_min + h)
+    cv2.rectangle(img, (x_min, y_min), (x_max, y_max), color=color, thickness=thickness)
+    ((text_width, text_height), _) = cv2.getTextSize(class_name, cv2.FONT_HERSHEY_SIMPLEX, 0.6, 1)    
+    cv2.rectangle(img, (x_min, y_min - int(1.5 * text_height)), (x_min + text_width, y_min), color, -1)
+    cv2.putText(img, class_name, (x_min, y_min - int(0.3 * text_height)), cv2.FONT_HERSHEY_SIMPLEX, 0.6,
+                text_color, thickness=2, lineType=cv2.LINE_AA)
+    return img
 
 def convert_mono_to_jpg(fromfile, tofile):
     """Convert monochrome image to color jpeg format.
