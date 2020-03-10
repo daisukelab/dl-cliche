efda72e57d7e5f6cb09642f8925c6b13f4d74eb4
daisukelab <niz.edge@gmail.com>
2020-03-10 14:37:22 +0900

    Update 0.1.3

--- setup.py
+++ setup.py
@@ -7,13 +7,14 @@ with open("README.md", "r") as f:
 
 setup(
     name='dl-cliche',
-    version='0.1.2',
+    version='0.1.3',
     description='dl-cliche: Packaging cliche utilities',
     long_description=readme,
     long_description_content_type="text/markdown",
     author='daisukelab',
     author_email='contact.daisukelab@gmail.com',
-    install_requires=['numpy', 'pandas', 'matplotlib', 'tqdm', 'easydict', 'imbalanced-learn'],
+    install_requires=['numpy', 'pandas', 'matplotlib', 'tqdm',
+                      'easydict', 'imbalanced-learn', 'tables', 'openpyxl'],
     url='https://github.com/daisukelab/dl-cliche',
     license=license,
     packages=find_packages(exclude=('test', 'docs')),
