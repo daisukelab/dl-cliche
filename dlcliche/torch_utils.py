d9954c12e9923bc5a9446b75a4b8e9b30a161c91
daisukelab <niz.edge@gmail.com>
2020-04-22 13:58:44 +0900

    Update 0.1.9

--- dlcliche/torch_utils.py
+++ dlcliche/torch_utils.py
@@ -1,4 +1,6 @@
 from .utils import *
+import numpy as np
+import torch
 import torch.nn as nn
 
 
@@ -20,6 +22,85 @@ class LossFlooding(nn.Module):
         loss = self.criteria(input, target)
         loss = (loss - self.b).abs() + self.b
         return loss
+
+
+class IntraBatchMixup():
+    """Mixup class that transform input and target, and runs loss calculation.
+    Works with every single batch, requires small changes with training process.
+
+    Example:
+        >>> batch_mixer = IntraBatchMixup(nn.CrossEntropyLoss(), alpha=your_alpha)
+        >>> # training
+        >>> for inputs, labels in train_loader:
+        >>>     inputs, stacked_labels = batch_mixer.transform(inputs, labels, train=True)
+        >>>     outputs = model(inputs)
+        >>>     loss = batch_mixer.criterion(outputs, stacked_labels)
+        >>> # validation
+        >>> for inputs, labels in val_loader:
+        >>>     inputs, stacked_labels = batch_mixer.transform(inputs, labels, train=False)
+        >>>     outputs = model(inputs)
+        >>>     loss = batch_mixer.criterion(outputs, stacked_labels)
+
+    Inspired by fast.ai: https://github.com/fastai/fastai/blob/master/fastai/callbacks/mixup.py
+        "Implements [mixup](https://arxiv.org/abs/1710.09412) training method"
+
+    Notes:
+        Batch size have to be more than 2, the larger the better w.r.t. mix diversity.
+    """
+
+    def __init__(self, criterion, alpha:float=0.4):
+        self.loss_func = _IntraBatchMixupLoss(criterion)
+        self.alpha = alpha
+
+    def transform(self, inputs, targets, train:bool):
+        "Applies mixup to `inputs` and `targets` if `train`."
+
+        if not train or self.alpha == 0.0:
+            return inputs, [targets, targets,
+                            torch.ones((targets.size(0)), dtype=torch.float).to(inputs.device)]
+
+        lambd = np.random.beta(self.alpha, self.alpha, targets.size(0))
+        lambd = np.concatenate([lambd[:,None], 1-lambd[:,None]], 1).max(1)
+        lambd = inputs.new(lambd)
+        shuffle = torch.randperm(targets.size(0)).to(inputs.device)
+
+        x1, y1 = inputs[shuffle], targets[shuffle]
+        out_shape = [lambd.size(0)] + [1 for _ in range(len(x1.shape) - 1)]
+
+        mixed_inputs = (inputs * lambd.view(out_shape) + x1 * (1-lambd).view(out_shape))
+        stacked_targets = [targets, y1, lambd]
+        return mixed_inputs, stacked_targets
+
+    def criterion(self, outputs, stacked_targets, outputs2=None):
+        return self.loss_func(outputs, stacked_targets, outputs2=outputs2)
+
+
+class _IntraBatchMixupLoss():
+    """Loss wrapper for Mixup.
+
+    Notes:
+        For use with IntraBatchMixup only, targets need to be stacked by the class.
+        Criterion cannot be reused; `reduction` attribute will be overwritten in this class.
+    """
+
+    def __init__(self, criterion):
+        assert hasattr(criterion, 'reduction'), 'Need usual loss function that have attr "reduction".'
+        assert criterion.reduction != 'none', 'Cannot reuse loss function, re-instantiate pls.'
+        # replace criterion's `reduction` with `none`, and keep the original setting.
+        self.criterion = criterion
+        self.reduction = criterion.reduction
+        setattr(self.criterion, 'reduction', 'none')
+
+    def __call__(self, outputs, stacked_targets, outputs2=None):
+        org_targets, counter_targets, lambd = stacked_targets
+        loss1 = self.criterion(outputs, org_targets.long())
+        loss2 = self.criterion(outputs if outputs2 is None else outputs2, counter_targets.long())
+        in_shape = [lambd.size(0)] + [1 for _ in range(len(outputs.shape) - 1)]
+        lambd = lambd.view(in_shape)
+        d = loss1 * lambd + loss2 * (1-lambd)
+        if self.reduction == 'mean':    return d.mean()
+        elif self.reduction == 'sum':   return d.sum()
+        return d
 
 
 def torch_show_trainable(model):
--- test/test_torch_utils.py
+++ test/test_torch_utils.py
@@ -41,6 +41,39 @@ class TestTorchUtils(unittest.TestCase):
         self.assertTrue(flooded == normal)
         self.assertTrue(b < normal)
 
+    def test_mixup(self):
+        N, C, H, W = 32, 3, 5, 10
+        # test data and fake preds
+        inputs = np.arange(N * C * H * W).reshape((N, C, H, W))
+        inputs = torch.tensor(inputs)
+        targets = np.arange(N)
+        targets = torch.tensor(targets)
+        preds = np.linspace(1.0, 0.0, num=N)
+        preds = [np.roll(preds, i) for i in range(N)]
+        preds = torch.tensor(preds)
+        # reference loss function
+        ref_crit = nn.CrossEntropyLoss(reduction='none')
+        # test both train/valid
+        for train, alpha in zip([True, True, True, False], [1.0, 0.5, 0.0, 0.0]):
+            for _ in range(1000):
+                batch_mixer = IntraBatchMixup(nn.CrossEntropyLoss(), alpha=alpha)
+                tfmed_inputs, stacked_targets = batch_mixer.transform(inputs, targets, train=train)
+                loss = batch_mixer.criterion(preds, stacked_targets)
+
+                org_targets, counterpart_targets, lambd = stacked_targets
+                shape = [lambd.size(0)] + [1 for _ in range(len(inputs.shape) - 1)]
+                in_lambd = lambd.view(shape)
+
+                calculated_inputs = inputs * in_lambd + inputs[counterpart_targets] * (1-in_lambd)
+                in_shape = [lambd.size(0)] + [1 for _ in range(len(inputs.shape) - 1)]
+                lambd = lambd.view(in_shape)
+                calculated_loss = ref_crit(preds, org_targets) * lambd + ref_crit(preds, counterpart_targets) * (1 - lambd)
+                calculated_loss = calculated_loss.mean()
+
+                self.assertTrue(np.all(0.0 <= lambd.numpy()) and np.all(lambd.numpy() <= 1.0))
+                self.assertTrue(np.all(calculated_inputs.numpy() == tfmed_inputs.numpy()))
+                self.assertTrue(np.all(calculated_loss.numpy() == loss.numpy())), f'{calculated_loss} != {loss}'
+
 
 if __name__ == '__main__':
     unittest.main()
