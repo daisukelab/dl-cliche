9175d373e042bfcb2df63d5c3865fcd08948f44e
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 06:52:31 +0000

    Adding numeric_owner as keyword arguement

--- dlcliche/data.py
+++ dlcliche/data.py
@@ -130,7 +130,7 @@ def extract_archive(from_path, to_path=None, remove_finished=False):
                     if not is_within_directory(path, member_path):
                         raise Exception("Attempted Path Traversal in Tar File")
             
-                tar.extractall(path, members, numeric_owner) 
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
                 
             
             safe_extract(tar, path=to_path)
@@ -152,7 +152,7 @@ def extract_archive(from_path, to_path=None, remove_finished=False):
                     if not is_within_directory(path, member_path):
                         raise Exception("Attempted Path Traversal in Tar File")
             
-                tar.extractall(path, members, numeric_owner) 
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
                 
             
             safe_extract(tar, path=to_path)
@@ -174,7 +174,7 @@ def extract_archive(from_path, to_path=None, remove_finished=False):
                     if not is_within_directory(path, member_path):
                         raise Exception("Attempted Path Traversal in Tar File")
             
-                tar.extractall(path, members, numeric_owner) 
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
                 
             
             safe_extract(tar, path=to_path)
